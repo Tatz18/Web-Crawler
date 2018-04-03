bbfb8b0fdfe7b81918c50e2b9bad65dfe771e942
Sayantan <sayantandatta97@gmail.com>
2018-04-04 02:59:32 +0530

    Update link-dump.py

--- link-dump.py
+++ link-dump.py
@@ -2,9 +2,9 @@ import urllib.request,urllib.parse, urllib.error
 from bs4 import BeautifulSoup as bs
 
 url = input("Enter the url >>> ")
-html = urllib.request.urlopen(url).read()
-soup = bs(html,'html.parser')
+html = urllib.request.urlopen(url).read()  #reading all html data
+soup = bs(html,'html.parser')               #parsing html using bs4
 
-tags = soup('a')
+tags = soup('a')			#finding & listing all anchor tags
 for tag in tags:
 	print(tag.get('href',None))
