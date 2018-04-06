8efb6747babc90818db5eab3aa6b388f2f19eea3
Sayantan <sayantandatta97@gmail.com>
2018-04-06 23:55:29 +0530

    Create json-url.py

---
+++ json-url.py
@@ -0,0 +1,19 @@
+import urllib.request as ur
+import urllib.parse as up
+import json
+
+# for manually input the url json_url = input("Enter the url >>> ")
+json_url = "https://python-data"
+
+print('Retrieving ', json_url)
+data = ur.urlopen(json_url).read().decode("utf-8")
+print("Retrieved ", len(data),'characters')
+json_obj = json.loads(data)
+
+sum = 0
+total_number = 0
+for comment in json_obj["comments"]:
+    sum +=  int(comment["count"])
+    total_number += 1
+print("Count ", total_number)
+print("Sum : ", sum)
